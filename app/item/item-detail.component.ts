3de83bbbb7244be86614b9d6f6d0d43d4632b526
Irina <irinaudovyk@gmail.com>
2017-05-19 18:55:03 +0300

    - added back button to item-detail view;

--- app/item/item-detail.component.ts
+++ app/item/item-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import {Location} from "@angular/common";
-// import { TNSFontIconService } from 'nativescript-ng2-fonticon';
 
 import { Item } from "./item";
 import { ItemService } from "./item.service";
@@ -18,7 +17,6 @@ export class ItemDetailComponent implements OnInit {
         private itemService: ItemService,
         private route: ActivatedRoute,
         private location: Location,
-        // private fonticon: TNSFontIconService
     ) { }
 
     ngOnInit(): void {
